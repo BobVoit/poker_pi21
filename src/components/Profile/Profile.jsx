acf50f24aba324af43e2ca49367d65eb2b9d8efc
KartohaWhy <38588352+KartohaWhy@users.noreply.github.com>
2020-12-01 17:03:30 +0400

    Add files via upload

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import nullAvatar from '../../images/nullAvatar.jpg';
 import greencoin from '../../images/green-coin.png';
+import blackcoin from '../../images/black-dollar.png';
+import redcross from '../../images/red-cross.png';
+import greenmark from '../../images/green-mark.png';
+import calendar from '../../images/calendar.png';
 
 
 
@@ -25,18 +29,29 @@ const Profile = () => {
                 <div className="profile-container_stats_header">
                     Статистика
                     </div>
-                    <div>Количество побед:</div>
-                    <div>Количество поражений:</div>
-                    <div className="profile-container_stats_money">
+                    <div className="profile-container_stats_win">
                     <img src={greencoin} alt="money"/>
-                    <div>Общее количество выигранных денег:</div>
+                        
+                            <div>Общее количество выигранных денег:</div>
                     </div>
-                    <div className="profile-container_stats_money_lose">
-                    
-                    <div>Общее количество проигранных денег:</div>
+                    <div className="profile-container_stats_loss"> 
+                        <img src={blackcoin} alt="blackcoin"/>
+                            <div>Общее количество проигранных денег:</div> 
                     </div>
-                    <div>Дата регистрации:</div>
                     
+                    <div className="profile-container_stats_biggest_win">
+                        <img src={greenmark} alt="greenmark"/>
+                            <div>Наибольший выигрыш:</div>
+                    </div>
+
+                    <div className="profile-container_stats_biggest_loss">
+                        <img src={redcross} alt="redcross"/>
+                            <div>Наибольший проигрыш:</div>
+                    </div>
+                    <div className="profile-container_stats_date_registration">
+                        <img src={calendar} alt="calendar"/>
+                            <div>Дата регистрации:</div>
+                    </div>
                 </div>
             </div>
         </div>
